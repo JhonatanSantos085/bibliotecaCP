bb76b4b5077cb4bfff0ebb3fcb3269461b79833f
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-11-23 17:11:51 -0300

    Criei os metodos e atributos da classe Endereco

--- src/main/java/Endereco.java
+++ src/main/java/Endereco.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
+
 public class Endereco {
+    private String rua;
+    private int numero;
+    private String bairro;
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void local() {
+        System.out.println("-_ Digite seu Endereco _-");
+        System.out.print("Digite o nome da rua: ");
+        setRua(scanner.nextLine());
+
+        System.out.print("Digite o numero da sua casa: ");
+        setNumero(Integer.parseInt(scanner.nextLine())); // Consumir a nova linha residual
+
+        System.out.print("Digite o seu Bairro: ");
+        setBairro(scanner.nextLine());
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 }
