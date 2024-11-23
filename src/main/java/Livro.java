621c940cb567db020f0c41c2cd1fc043d9575188
Gabriel <gabrielrozendodacosta@gmail.com>
2024-11-23 18:32:37 -0300

    Criei os atributos e métodoss da classe Livro

--- src/main/java/Livro.java
+++ src/main/java/Livro.java
@@ -1,2 +1,41 @@
 public class Livro {
+
+    private String titulo;
+    private String genero;
+    private String autor;
+
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "Titulo='" + titulo + '\'' +
+                ", Gênero='" + genero + '\'' +
+                ", Autor='" + autor + '\'' +
+                '}';
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
 }
