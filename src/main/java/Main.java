4841e2da092e85e0bc91b11d141d7ce1ca227f36
Gabriel <gabrielrozendodacosta@gmail.com>
2024-12-03 20:28:08 -0300

    Criei método de cadastro de livro

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,6 +33,8 @@ public class Main {
                     break;
                 case 3:
                     System.out.println("Cadastrando livro");
+                    Livro livro = new Livro();
+                    livro.cadastrarLivro();
                     break;
                 case 4:
                     System.out.println("Mostrando os dados cadastrados");
